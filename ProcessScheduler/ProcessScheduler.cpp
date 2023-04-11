03b6cd235e301338c24ad8261778960d2c7f6ccd
Samy <abdelruhman.mohamad04@eng-st.cu.edu.eg>
2023-04-11 03:56:06 +0200

    sss

--- ProcessScheduler/ProcessScheduler.cpp
+++ ProcessScheduler/ProcessScheduler.cpp
@@ -1,38 +1,21 @@
 // ProcessScheduler.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-#include"Queue.h"
-#include"PriQqueue.h"
-#include"PriorityQueue.h"
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    PriorityQueue<int> Q;
-    
-    Q.Enqueue(5, 3);
-    Q.Enqueue(2 , 3);
-    Q.Enqueue(4, 10);
-    Q.Enqueue(1,0);    
-    Q.Enqueue(8,5);
-    Q.Print();
-    cout << endl;
-    int a , b;
-    Q.Dequeue();
-    Q.Print();
-    cout << endl;
-
-    Q.Dequeue();
-    Q.Print();
-    cout << endl;
-
-    Q.Dequeue();
-    Q.Print();
-    cout << endl;
-
-    Q.QueueFront(a, b);
-    cout << a << " " << b;
-    
-   
+    std::cout << "Hello World!\n";
 }
 
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
